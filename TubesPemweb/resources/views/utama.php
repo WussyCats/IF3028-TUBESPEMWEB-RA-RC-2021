011f11be8365e88e79242769d2e4bad5318c6a5d
URuGuAY <82500240+WussyCats@users.noreply.github.com>
2021-12-09 09:44:56 +0700

    Create utama.php
    Tampilan utama (Not Fixed)

---
+++ TubesPemweb/resources/views/utama.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="utama.css">
+        <title> SIMPLE LAPOR! | TUBES PEMWEB RA </title>
+    </head>
+
+    <body>
+        <div class="container">
+            <header>
+                <h1> SIMPLE LAPOR! </h1>
+                <div class="search-container">
+                    <input type="text" name="search" placeholder="Search..">
+                    <button type ="submit"> Cari </button>
+                </div>
+            </header>
+        
+            <header>
+                <div class="add-container">
+                    <a> Buat Laporan/Komentar </a>
+                </div>
+            </header>
+            <br>
+
+            <div class="lapor-container">
+                <p> Laporan/Komentar Terakhir </p>
+                <hr>
+            </div>
+        </div>
+    </body>
