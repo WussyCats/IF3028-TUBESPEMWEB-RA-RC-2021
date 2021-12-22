6f8f20c9e4250c0a23103d89bbdff02d7a645f5b
Muhammad Farisi Zatwara Putra Unyi <82500240+WussyCats@users.noreply.github.com>
2021-12-23 03:19:01 +0700

    Tambah tampilan utama dan edit

---
+++ TubesPemweb/resources/views/show2.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="{!!asset('assets/css/buat.css')!!}">
+    <title>SIMPLE LAPOR!</title>
+    <style>
+        option[value=dosen, value=mahasiswa, value=mahasiswa, value=infrastruktur, value=pengajar] {
+            width: 100%;
+            padding: 8px 15px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 2px solid black;
+            box-sizing: border-box;
+            height: 50px;
+            font-size: 20px;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <hr>
+    <div class="content">
+        <div class="judul">
+            <center>
+                <h1>SIMPLE LAPOR!</h1>
+            </center>
+        </div>
+        <div class="container">
+            <form action="{{url('/update/'.$data->id)}}" method="POST" enctype="multipart/form-data">
+                @csrf
+
+                <label for="isi">Update Laporan/Komentar</label><br>
+                <hr>
+                <textarea type="text" id="isi" name="isi_laporan" placeholder="Laporan/Komentar"></textarea>
+                <br><br>
+                <select name="tipe_laporan" id="tipe">
+                    <option hidden>Pilih Aspek Pelaporan/Komentar</option>
+                    <option value="dosen">Dosen</option>
+                    <option value="staff">Staff</option>
+                    <option value="mahasiswa">Mahasiswa</option>
+                    <option value="infrastruktur">Infrastruktur</option>
+                    <option value="pengajaran">Pengajaran</option>
+                </select>
+                <br><br>
+                <input type="date" id="tanggal" name="tgl_laporan" placeholder="dd/mm/yy">
+                <br><br>
+                <input type="file" name="lampiran_laporan" id="lampiran">
+                <button type="submit">Update LAPOR!</button>
+                
+            </form>
+
+        </div>
+    </div>
+
+</body>
